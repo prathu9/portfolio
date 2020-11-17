01f9f8e40ce364e87d501030e8b805c5997868ad
prathamesh sawant <prathameshpgs@gmail.com>
2020-11-18 00:10:31 +0530

    Added dark mode

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,62 @@
+(function(){
+	const toggleSwitch = document.querySelector("input[type='checkbox']");
+	const toggleIcon =  document.getElementById("toggle-icon");
+	//Dark Mode Styles
+	const darkMode = ()=>{
+	    // nav.style.backgroundColor = "rgb(0 0 0/50%)";
+	    toggleIcon.children[0].textContent = "Dark";
+	    toggleIcon.children[1].classList.replace("fa-sun","fa-moon");
+	    // imageMode("dark");
+	    // textBox.style.backgroundColor = "rgb(255 255 255/50%)";
+	}
+
+	//Light Mode Styles
+	const lightMode = ()=>{
+	    // nav.removeAttribute("style");
+	    toggleIcon.children[0].textContent = "Light";
+	    toggleIcon.children[1].classList.replace("fa-moon","fa-sun");
+	    // imageMode("light");
+	    // textBox.removeAttribute("style");
+	}
+
+
+	// const imageMode = (color)=>{
+	//     image1.src=`image/undraw_Online_world_re_h4cb_${color}.svg`;
+	//     image2.src=`image/undraw_Mail_sent_re_0ofv_${color}.svg`;
+	//     image3.src=`image/undraw_camping_noc8_${color}.svg`;
+	// }
+
+	//Check Local Storage for Theme
+	(()=>{
+	    const currentTheme = localStorage.getItem("theme");
+	    if(currentTheme){
+	        document.documentElement.setAttribute("data-theme", currentTheme);
+	        if(currentTheme==="dark"){
+	            toggleSwitch.checked = true;
+	            darkMode();
+	        }
+	    }
+	})();
+
+	//Change Theme Dynamically
+	const changeTheme = (event)=>{
+	    if(event.target.checked){
+	        document.documentElement.setAttribute("data-theme", "dark");
+	        darkMode();
+	        localStorage.setItem("theme","dark");
+	    }
+	    else{
+	        document.documentElement.setAttribute("data-theme", "light");
+	        lightMode();
+	        localStorage.setItem("theme","light");
+	    }   
+	}
+
+	//Event Listener
+	toggleSwitch.addEventListener("change", changeTheme);
+})();
+
+
 const focusOnScroll = (item, index)=>{
 	item[0].className = index === 0?"active":"";
 	item[1].className = index === 1?"active":"";
