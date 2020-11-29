5865f39be2f4f36ff6996346a5ec45e3d0a7daa7
prathamesh sawant <prathameshpgs@gmail.com>
2020-11-29 15:24:33 +0530

    Added project content and styling in project page

--- js/modify-logo.js
+++ js/modify-logo.js
@@ -3,7 +3,7 @@ const _modifyLogo = (()=>{
 
 	//Check local storage for theme and change logo according to theme
 	return ((imageMode)=>{
-		const currentTheme = localStorage.getItem("theme");
+		const currentTheme = localStorage.getItem("theme")||"light";
 		if(currentTheme==="dark" && imageMode){
 			imageMode("dark");
 		}else if(currentTheme==="light" && imageMode){
