f00f4c685367ce9cdacc11a843a39eaccd03b7e2
prathamesh sawant <prathameshpgs@gmail.com>
2020-12-04 00:34:11 +0530

    Added responsive image for desktop and mobile view in project page

--- js/project.js
+++ js/project.js
@@ -7,3 +7,21 @@
 	//change logo when light and dark mode switch is toggled
 	toggleSwitch.addEventListener("change", ()=>{changeLogo()}); 
 })();
+
+(function(){
+	const images = document.querySelectorAll(".project-image img");
+	let x=window.matchMedia("(max-width:784px)");
+	function changeImage(x){
+		if(x.matches){
+			images[0].src = `../images/music-player-mobile.jpg`;
+			images[1].src = `../images/animated-template-mobile.jpg`;
+			images[2].src = `../images/tic-tac-toe-mobile.jpg`;
+		}else{
+			images[0].src = `../images/music-player-desktop.jpg`;
+			images[1].src = `../images/animated-template-desktop.jpg`;
+			images[2].src = `../images/tic-tac-toe-desktop.jpg`;
+		}	
+	}
+	console.log("hello");
+	x.addListener(changeImage);
+})();
