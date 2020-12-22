7d04ecd0877813146404b136bf6809a2b46d672f
prathamesh sawant <prathameshpgs@gmail.com>
2020-12-22 23:36:22 +0530

    Added joke teller project

--- js/project.js
+++ js/project.js
@@ -17,11 +17,13 @@
 			images[1].src = `../images/animated-template-mobile.jpg`;
 			images[2].src = `../images/tic-tac-toe-mobile.jpg`;
 			images[3].src = `../images/quote-generator-mobile.jpg`;
+			images[4].src = `../images/joke-teller-mobile.jpg`;
 		}else{
 			images[0].src = `../images/music-player-desktop.jpg`;
 			images[1].src = `../images/animated-template-desktop.jpg`;
 			images[2].src = `../images/tic-tac-toe-desktop.jpg`;
 			images[3].src = `../images/quote-generator-desktop.jpg`;
+			images[4].src = `../images/joke-teller-desktop.jpg`;
 		}	
 	}
 	console.log("hello");
