09aa736b94c646261b006fd525dcd0485703ecdf
prathamesh sawant <prathameshpgs@gmail.com>
2021-01-16 21:18:28 +0530

    Removed alertDisplay js file

--- js/alertDisplay.js
+++
@@ -1,8 +0,0 @@
-(function(){
-	const isAlertDisplayed = sessionStorage.getItem("isAlertDisplayed");
-	if(!isAlertDisplayed){
-		alert("Currently working on a React project!");
-	}
-	
-	sessionStorage.setItem("isAlertDisplayed", true);
-})();
