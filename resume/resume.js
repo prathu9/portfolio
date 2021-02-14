2ea7f98bc3e9cca577288b1478c64b9496a5f0a9
prathamesh sawant <prathameshpgs@gmail.com>
2021-02-14 11:18:14 +0530

    Changed firebase bucket for downloading resume.

--- resume/resume.js
+++ resume/resume.js
@@ -1,42 +1,44 @@
-// Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyAQLQ_lgc9japqgy4u8S6Mq-X0wpdb9y4o",
-  authDomain: "resume-b728a.firebaseapp.com",
-  projectId: "resume-b728a",
-  storageBucket: "resume-b728a.appspot.com",
-  messagingSenderId: "301251047445",
-  appId: "1:301251047445:web:cd636e5fd4660fc6041e1c"
-};
-  
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-/* Get a reference to the storage service, 
-which is used to create references in your storage bucket*/
-var storage = firebase.storage();
-
-// Create a storage reference from our storage service
-var storageRef = storage.ref();
-
-var resume = storageRef.child("resume.pdf");
-
-
-(()=>{resume.getDownloadURL()
-  .then((url) => {
-    const downloadBtn = document.querySelector(".download-btn");
-    downloadBtn.href=url;
-    downloadBtn.target="blank";
-    console.log(downloadBtn);
-  })
-  .catch((error) => {
-    // Handle any errors
-    console.log(error);})
-})();
-
 (()=>{
-  const downloadPopUp = document.querySelector(".download-popup");
-  setTimeout(()=>{downloadPopUp.style.display="block"},0);
-  setTimeout(()=>{downloadPopUp.style.display="none"},2000);
-})();
+  // Firebase configuration
+  var firebaseConfig = {
+        apiKey: "AIzaSyArBm5el849hwEozoGHd67XMGt2N7miXv8",
+        authDomain: "portfolio-project-608d1.firebaseapp.com",
+        databaseURL: "https://portfolio-project-608d1.firebaseio.com",
+        projectId: "portfolio-project-608d1",
+        storageBucket: "portfolio-project-608d1.appspot.com",
+        messagingSenderId: "53294103964",
+        appId: "1:53294103964:web:ffb1fa7426d39ca0b25c59"
+  };
+    
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
+  /* Get a reference to the storage service, 
+  which is used to create references in your storage bucket*/
+  var storage = firebase.storage();
+
+  // Create a storage reference from our storage service
+  var storageRef = storage.ref();
+
+  var resume = storageRef.child("resume.pdf");
+
+
+  (()=>{resume.getDownloadURL()
+    .then((url) => {
+      const downloadBtn = document.querySelector(".download-btn");
+      downloadBtn.href=url;
+      downloadBtn.target="blank";
+      console.log(downloadBtn);
+    })
+    .catch((error) => {
+      // Handle any errors
+      console.log(error);})
+  })();
+
+  (()=>{
+    const downloadPopUp = document.querySelector(".download-popup");
+    setTimeout(()=>{downloadPopUp.style.display="block"},0);
+    setTimeout(()=>{downloadPopUp.style.display="none"},2000);
+  })();
+})();
 
